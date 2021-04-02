38cddd9b65ef5a6ec09e7d7230dea21537b32540
SupeRodv2 <rod@protobia.tech>
2021-04-03 00:29:57 +0800

    remote api

--- utils/env.js
+++ utils/env.js
@@ -1,4 +1,6 @@
-const apiDomain = 'http://192.168.31.222';
+// const apiDomain = 'http://192.168.31.222';
+const apiDomain = 'http://hc.dev.protobia.net';
+
 const apiProHost =  apiDomain + '/api';
 
 module.exports = {
