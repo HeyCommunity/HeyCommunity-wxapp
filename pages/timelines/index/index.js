8fb70d4c10ec2b942fbad0c82e555576b3aac796
SupeRodv2 <rod@protobia.tech>
2021-04-03 22:49:18 +0800

    timeline index page update

--- pages/timelines/index/index.js
+++ pages/timelines/index/index.js
@@ -16,4 +16,25 @@ Page({
       _this.setData({timelines: data});
     });
   },
+
+  /**
+   * goto 发布动态页面
+   */
+  gotoCreatePage() {
+    wx.navigateTo({url: '/pages/timelines/create/index'});
+  },
+
+  /**
+   * 下拉刷新
+   */
+  onPullDownRefresh() {
+    let _this = this;
+    _this.setData({timelines: []});
+
+    // 获取动态
+    HTTP.httpGet('timelines', {}, function(data) {
+      _this.setData({timelines: data});
+      wx.stopPullDownRefresh();
+    });
+  },
 });
