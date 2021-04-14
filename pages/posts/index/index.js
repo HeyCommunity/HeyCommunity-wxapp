171f12804de1184da9c05cd05108dfd040f7b123
SupeRodv2 <rod@protobia.tech>
2021-04-15 02:40:39 +0800

    thumbUp feature

--- pages/posts/index/index.js
+++ pages/posts/index/index.js
@@ -37,6 +37,32 @@ Page({
   },
 
   /**
+   * 点赞
+   */
+  thumbUpHandler(event) {
+    let _this = this;
+    let postIndex = event.currentTarget.dataset.postIndex;
+    let postId = event.currentTarget.dataset.postId;
+    let type = event.currentTarget.dataset.type;
+    let value = event.currentTarget.dataset.value;
+
+    let params = {
+      post_id: postId,
+      type: type,
+      value: value,
+    };
+
+    HTTP.httpPost('post-thumbs', params, function(data) {
+      _this.data.posts[postIndex] = data;
+      _this.setData({posts: _this.data.posts});
+
+      let title = '点赞成功';
+      if (! data.i_have_thumb_up) title = '取消点赞';
+      wx.showToast({title: title, icon: 'none'});
+    });
+  },
+
+  /**
    * 下拉刷新
    */
   onPullDownRefresh() {
