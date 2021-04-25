e1a8081635b0f39a632bf804eb635bf6a3ae9c92
SupeRodv2 <rod@protobia.tech>
2021-04-26 00:01:29 +0800

    change api domain

--- utils/env.js
+++ utils/env.js
@@ -1,5 +1,6 @@
-const apiDomain = 'http://192.168.31.111';
+// const apiDomain = 'http://192.168.31.111';
 // const apiDomain = 'https://hc.dev.protobia.net';
+const apiDomain = 'https://api.hey-community.com.cn';
 
 const apiProHost =  apiDomain + '/api';
 
