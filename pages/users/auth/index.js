5159a7fda9d9bd3bfe58e7cc51606bb897784cf0
SupeRodv2 <rod@protobia.tech>
2021-04-29 16:06:07 +0800

    auth optimization

--- pages/users/auth/index.js
+++ pages/users/auth/index.js
@@ -1,4 +1,3 @@
-const AUTH = require('../../../utils/auth.js');
 const APP = getApp();
 
 Page({
@@ -27,19 +26,19 @@ Page({
       desc: '登录小程序',
       success: (res) => {
         let wechatUserInfo = res.userInfo;
-        getApp().globalData.wechatUserInfo = wechatUserInfo;
+        APP.globalData.wechatUserInfo = wechatUserInfo;
 
         wx.login({
           success: res => {
             // 通过 wx.login res.code 发送到后台, 从而完成用户的注册和登录
-            AUTH.userLogin(res.code).then(function() {
+            APP.AUTH.userLogin(res.code).then(function() {
               // 登录成功后更新用户资料
               // TODO: 不要自动更新用户资料，而是在我的页面让用户手动触发
-              AUTH.userUpdateInfo(wechatUserInfo);
+              APP.AUTH.userUpdateInfo(wechatUserInfo);
 
               wx.navigateBack({
                 success(res) {
-                  getApp().Notify({type: 'success', message: '登录成功'});
+                  APP.Notify({type: 'success', message: '登录成功'});
                 }
               });
             }).catch(function() {
