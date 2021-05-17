d905c211af0d507209f0982d7f4e3d2890f6756d
SupeRodv2 <rod@protobia.tech>
2021-05-17 13:25:23 +0800

    env local support

---
+++ env.local.example.js
@@ -0,0 +1,10 @@
+const prodApiDomain = 'https://dev.hey-community.com.cn';
+const trailApiDomain = 'https://dev.hey-community.com.cn';
+const devApiDomain = 'http://127.0.0.1:8000';
+
+// 设定 API
+const apiDomain = prodApiDomain;
+
+module.exports = {
+  apiDomain,
+};
--- utils/env.js
+++ utils/env.js
@@ -1,17 +1,18 @@
-const prodApiDomain = 'https://demo.hey-community.com.cn';
-const devApiDomain = 'http://192.168.31.111';
+let apiDomain;
+const prodApiDomain = 'https://dev.hey-community.com.cn';
+const devApiDomain = 'http://127.0.0.1:8000';
 
 // 配置 apiDomain，默认使用 prodApiDomain
-let apiDomain = prodApiDomain;
+apiDomain = prodApiDomain;
 
-// 如果是开发版则使用 devApiDomain
-let wxAccountInfo = wx.getAccountInfoSync();
-if (wxAccountInfo.miniProgram.envVersion === 'develop') {
-  apiDomain = devApiDomain;
+// 如果存在 env.local.js，则使用这个文件的 apiDomain
+try {
+  let envLocal = require('../env.local.js');
+  apiDomain = envLocal.apiDomain;
+  console.debug('使用 env.local.js apiDomain: ' + apiDomain);
+} catch (exception) {
+  console.debug('使用 utils/env.js apiDomain: ' + apiDomain);
 }
-
-// FOR DEV: 强制使用 prodApiDomain
-// apiDomain = prodApiDomain;
 
 const apiProHost =  apiDomain + '/api';
 module.exports = {
