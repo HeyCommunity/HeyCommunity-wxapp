856f2821c0b763da2f2094311921e80b8553ee7f
SupeRodv2 <rod@protobia.tech>
2021-05-28 02:54:27 +0800

    fix env

--- env.local.example.js
+++ env.local.example.js
@@ -1,13 +1,17 @@
 // App Config
 const appName = 'HEY社区';
 
-// 设定 API
+// API list
 const prodApiDomain = 'https://dev.hey-community.com.cn';
-const trailApiDomain = 'https://dev.hey-community.com.cn';
-const devApiDomain = 'http://127.0.0.1:8000';
+const trialApiDomain = 'https://dev.hey-community.com.cn';
+const devApiDomain = 'https://dev.hey-community.com.cn';
+const localApiDomain = 'http://127.0.0.1:8000';
+
+// 设定 API
 let apiDomain = prodApiDomain;
-// apiDomain = trailApiDomain;
+// apiDomain = trialApiDomain;
 // apiDomain = devApiDomain;
+// apiDomain = localApiDomain;
 
 module.exports = {
   apiDomain,
