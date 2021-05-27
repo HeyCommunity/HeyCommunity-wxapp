3fa8fbd53c9aa8eb26a3ad53e65e8fc50548d28c
SupeRodv2 <rod@protobia.tech>
2021-05-28 01:36:15 +0800

    modify  env.local.example.js

--- env.local.example.js
+++ env.local.example.js
@@ -6,8 +6,8 @@ const prodApiDomain = 'https://dev.hey-community.com.cn';
 const trailApiDomain = 'https://dev.hey-community.com.cn';
 const devApiDomain = 'http://127.0.0.1:8000';
 let apiDomain = prodApiDomain;
-// apidomain = trailapidomain;
-// apidomain = devapidomain;
+// apiDomain = trailApiDomain;
+// apiDomain = devApiDomain;
 
 module.exports = {
   apiDomain,
