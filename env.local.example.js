ec511651ab572bf46f626a1338391dfbe537f22b
SupeRodv2 <rod@protobia.tech>
2021-05-27 23:13:39 +0800

    modify  env.local.example.js

--- env.local.example.js
+++ env.local.example.js
@@ -5,7 +5,9 @@ const appName = 'HEY社区';
 const prodApiDomain = 'https://dev.hey-community.com.cn';
 const trailApiDomain = 'https://dev.hey-community.com.cn';
 const devApiDomain = 'http://127.0.0.1:8000';
-const apiDomain = prodApiDomain;
+let apiDomain = prodApiDomain;
+// apidomain = trailapidomain;
+// apidomain = devapidomain;
 
 module.exports = {
   apiDomain,
