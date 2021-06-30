fcaecf46448ded5a2b485289d448cdee5b4f9e33
SupeRodv2 <rod@protobia.tech>
2021-07-01 00:08:25 +0800

    refactor(API): change api domain

--- env.local.example.js
+++ env.local.example.js
@@ -2,14 +2,12 @@
 const appName = 'HEY社区';
 
 // API list
-const prodApiDomain = 'https://dev.hey-community.com.cn';
-const trialApiDomain = 'https://dev.hey-community.com.cn';
-const devApiDomain = 'https://dev.hey-community.com.cn';
+const prodApiDomain = 'https://dev.api.heycommunity.com';
+const devApiDomain = 'https://dev.api.heycommunity.com';
 const localApiDomain = 'http://127.0.0.1:8000';
 
 // 设定 API
 let apiDomain = prodApiDomain;
-// apiDomain = trialApiDomain;
 // apiDomain = devApiDomain;
 // apiDomain = localApiDomain;
 
