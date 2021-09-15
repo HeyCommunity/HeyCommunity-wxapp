71bf3db9edecbebe29b111afbc6a3b4ba26f1482
SupeRodv2 <rod@protobia.tech>
2021-09-15 16:17:45 +0800

    feat(UserDetailPage): 用户主页显示自己的动态

--- pages/users/detail/index.js
+++ pages/users/detail/index.js
@@ -4,11 +4,18 @@ const APP = getApp();
 Page({
   data: {
     appGlobalData: null,
+    currentPage: 1,
+    lastPage: null,
+    refreshLoading: false,
+    moreLoading: false,
+
     defaultUserCoverImagePath: apiDomain + '/images/users/default-cover.jpg',
     defaultProfileWaveImagePath: apiDomain + '/images/users/profile-wave.gif',
 
     userId: null,
     userInfo: null,
+
+    posts: [],
   },
 
   /**
@@ -25,7 +32,10 @@ Page({
    */
   onShow() {
     this.setData({appGlobalData: APP.globalData});
+
     this.getUserInfo();
+
+    this.getPosts();
   },
 
   /**
@@ -41,5 +51,62 @@ Page({
         title: result.data.nickname,
       });
     });
+  },
+
+  /**
+   * 获取 posts
+   */
+  getPosts(pageNum) {
+    let _this = this;
+    if (! pageNum) pageNum = _this.data.currentPage;
+
+    return new Promise(function(resolve, reject) {
+      APP.HTTP.GET('users/' + _this.data.userId + '/posts', {page: pageNum}).then(function(result, res) {
+        if (result.meta.current_page === 1) _this.data.posts = [];
+        _this.data.posts = _this.data.posts.concat(result.data);
+        _this.setData({posts: _this.data.posts});
+
+        _this.setData({
+          currentPage: result.meta.current_page,
+          lastPage: result.meta.last_page,
+        });
+
+        resolve(result, res);
+      }).catch(function(result, res) {
+        reject(result, res);
+      });
+    });
+  },
+
+  /**
+   * 下拉刷新
+   */
+  onPullDownRefresh() {
+    let _this = this;
+
+    wx.showLoading({title: '刷新中'});
+    _this.getPosts(1).finally(function() {
+      wx.stopPullDownRefresh();
+      wx.hideLoading();
+    });
+  },
+
+  /**
+   * 下拉加载更多
+   */
+  onReachBottom() {
+    let _this = this;
+
+    if (this.data.currentPage >= this.data.lastPage) {
+      wx.showToast({icon: 'none', title: '没有更多数据了'});
+    } else {
+      // wx.showLoading({title: '加载中'});
+      this.setData({moreLoading: true});
+
+      this.getPosts(this.data.currentPage + 1).finally(function() {
+        // wx.hideLoading();
+        _this.setData({moreLoading: false});
+      });
+    }
   },
 });
