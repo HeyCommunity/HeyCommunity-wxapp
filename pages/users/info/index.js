7cf5d41a2a5ded852827434eb258895b47605247
SupeRodv2 <rod@protobia.tech>
2021-09-17 20:14:42 +0800

    feat(UserInfoPage): 用户资料更新

--- pages/users/info/index.js
+++ pages/users/info/index.js
@@ -7,6 +7,8 @@ Page({
     defaultUserCoverImagePath: apiDomain + '/images/users/default-cover.jpg',
 
     userInfo: null,
+    genderIndex: null,
+    genderRange: ['保密', '男', '女'],
   },
 
   /**
@@ -21,6 +23,44 @@ Page({
    */
   onShow() {
     this.getUserInfo();
+  },
+
+  /**
+   * 性别选择器 change 处理
+   */
+  genderPickerChange(event) {
+    let genderIndex = event.detail.value;
+    this.setData({genderIndex: genderIndex});
+  },
+
+  /**
+   * 更新我的资料
+   */
+  updateInfoHandler(event) {
+    let data = event.detail.value;
+
+    APP.HTTP.POST('users/mine-info', data).then(function(result) {
+      wx.showToast({
+        icon: 'success',
+        title: '更新资料成功',
+        mask: true,
+        duration: 1500,
+        complete(res) {
+          setTimeout(function() {
+            wx.navigateBack();
+          }, 1500);
+        },
+      });
+    }).catch(function(res) {
+      let errorMessage = '未知错误';
+      if (res.data.errors) errorMessage = res.data.errors[Object.keys(res.data.errors)[0]][0];
+
+      wx.showModal({
+        title: '更新资料失败',
+        content: errorMessage,
+        showCancel: false,
+      });
+    });
   },
 
   /**
