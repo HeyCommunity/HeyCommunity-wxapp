956e2a97c85b8274d0cf101fe16ec654a1430b61
SupeRodv2 <rod@protobia.tech>
2021-10-25 12:34:43 +0800

    optimize(UserDetailPage): 优化用户详情页

--- pages/users/detail/index.js
+++ pages/users/detail/index.js
@@ -1,20 +1,16 @@
-const {apiDomain} = require('../../../utils/env.js');
 const APP = getApp();
+const MODEL = require('../../../utils/model.js');
+const {apiDomain} = require('../../../utils/env.js');
 
 Page({
   data: {
     appGlobalData: null,
-    currentPage: 1,
-    lastPage: null,
-    refreshLoading: false,
-    moreLoading: false,
-
-    defaultProfileWaveImagePath: apiDomain + '/images/users/profile-wave.gif',
+    models: [],
+    posts: [],
 
     userId: null,
     userInfo: null,
-
-    posts: [],
+    defaultProfileWaveImagePath: apiDomain + '/images/users/profile-wave.gif',
   },
 
   /**
@@ -34,7 +30,8 @@ Page({
 
     this.getUserInfo();
 
-    this.getPosts();
+    MODEL.init(this, 'posts/user-posts?user_id=1');
+    MODEL.getFirstPageModels();
   },
 
   /**
@@ -53,40 +50,13 @@ Page({
   },
 
   /**
-   * 获取 posts
-   */
-  getPosts(pageNum) {
-    let _this = this;
-    if (! pageNum) pageNum = _this.data.currentPage;
-
-    return new Promise(function(resolve, reject) {
-      APP.HTTP.GET('users/' + _this.data.userId + '/posts', {page: pageNum}).then(function(result, res) {
-        if (result.meta.current_page === 1) _this.data.posts = [];
-        _this.data.posts = _this.data.posts.concat(result.data);
-        _this.setData({posts: _this.data.posts});
-
-        _this.setData({
-          currentPage: result.meta.current_page,
-          lastPage: result.meta.last_page,
-        });
-
-        resolve(result, res);
-      }).catch(function(result, res) {
-        reject(result, res);
-      });
-    });
-  },
-
-  /**
    * 下拉刷新
    */
   onPullDownRefresh() {
-    let _this = this;
-
     wx.showLoading({title: '刷新中'});
-    _this.getPosts(1).finally(function() {
-      wx.stopPullDownRefresh();
+    MODEL.getFirstPageModels().finally(function() {
       wx.hideLoading();
+      wx.stopPullDownRefresh();
     });
   },
 
@@ -94,19 +64,7 @@ Page({
    * 下拉加载更多
    */
   onReachBottom() {
-    let _this = this;
-
-    if (this.data.currentPage >= this.data.lastPage) {
-      wx.showToast({icon: 'none', title: '没有更多数据了'});
-    } else {
-      // wx.showLoading({title: '加载中'});
-      this.setData({moreLoading: true});
-
-      this.getPosts(this.data.currentPage + 1).finally(function() {
-        // wx.hideLoading();
-        _this.setData({moreLoading: false});
-      });
-    }
+    MODEL.getNextPageModels();
   },
 
   /**
